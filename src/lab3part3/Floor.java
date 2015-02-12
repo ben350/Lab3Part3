9c26e46b6bf6fb4237b38f3a6a4316b0d4a16775
benjamin.ramirez006 <benjamin.ramirez006@10.26.2.32>
2015-02-11 20:19:24 -0800

    part 3 in progress

---
+++ src/lab3part3/Floor.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3part3;
+
+/**
+ *
+ * @author benjamin.ramirez006
+ */
+
+public class Floor {
+  private int floor_num;
+  private int passengers_up;
+  private int passengers_down;
+  
+  
+  public Floor(int floor) {
+    floor_num = floor;
+    passengers_up = 0;
+    passengers_down = 0;
+  }
+  
+  
+  public void unloadPassengers(Elevator elev, boolean going_up) {
+    int passengers = going_up?passengers_up:passengers_down;
+    for (; passengers>0; passengers--) {
+      try {
+        elev.boardPassenger(1);
+      }
+      catch (ElevatorFullException e) {
+        System.out.println(e.getMessage());
+        elev.registerRequest(floor_num, going_up);
+        break;
+      }
+    }
+    
+    if (going_up) {
+      passengers_up = passengers;
+    }
+    else {
+      passengers_down = passengers;
+    }
+  }
+  
+  /**
+  * Testing function
+  */
+  public void addPassenger(boolean going_up) {
+    if (going_up) {
+      passengers_up++;
+    }
+    else {
+      passengers_down++;
+    }
+  }
+}
